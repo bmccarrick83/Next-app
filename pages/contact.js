e8293046f04d0979cf6cf595fe39dc08f41b4ce0
Brandon Edward Mccarrick <bmccarrick83@users.noreply.github.com>
2018-05-07 12:58:21 -0500

    pages

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+export default () => (
+  <div>
+    <p>
+      <a href="bmccarrick83@gmail.com">Contact me!</a>
+    </p>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/Link'
+
+export default () => (
+  <div>
+    <p>Hello, this is my first Next.js web app.</p>
+    <Link href="/contact">
+    <a>Contact me!</a>
+    </Link>
+  </div>
+)
